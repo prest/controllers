d8b98e79a4b9427c0ed094eda58cab9d935cbd76
Felipe Oliveira <felipeweb.programador@gmail.com>
2018-05-04 10:13:57 -0300

    debug

--- tables_test.go
+++ tables_test.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -245,6 +246,7 @@ func TestBatchInsertInTables(t *testing.T) {
 		if err != nil {
 			t.Error("error on ioutil ReadAll", err)
 		}
+		fmt.Println(">>>>>>>>>", string(body))
 		if tc.isCopy && len(body) != 0 {
 			t.Errorf("len body is %d", len(body))
 		}
