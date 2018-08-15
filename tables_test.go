5b858875fabd23fbe1b7598c8d6d6df282acee3d
julien brunet <julien@brunet.io>
2018-08-15 23:48:26 +0200

    Adding testing for returning clause

--- tables_test.go
+++ tables_test.go
@@ -292,19 +292,24 @@ func TestUpdateFromTable(t *testing.T) {
 		url         string
 		request     map[string]interface{}
 		status      int
+		body        string
 	}{
-		{"execute update in a table without custom where clause", "/prest/public/test", m, http.StatusOK},
-		{"execute update in a table with where clause", "/prest/public/test?name=$eq.nuveo", m, http.StatusOK},
-		{"execute update in a table with invalid database", "/0prest/public/test", m, http.StatusBadRequest},
-		{"execute update in a table with invalid schema", "/prest/0public/test", m, http.StatusBadRequest},
-		{"execute update in a table with invalid table", "/prest/public/0test", m, http.StatusBadRequest},
-		{"execute update in a table with invalid where clause", "/prest/public/test?0name=$eq.nuveo", m, http.StatusBadRequest},
-		{"execute update in a table with invalid body", "/prest/public/test?name=$eq.nuveo", nil, http.StatusBadRequest},
+		{"execute update in a table without custom where clause", "/prest/public/test", m, http.StatusOK, ""},
+		{"execute update in a table with where clause", "/prest/public/test?name=$eq.nuveo", m, http.StatusOK, ""},
+		{"execute update in a table with where clause", "/prest/public/test?name=$eq.nuveo&_returning=name", m, http.StatusOK, "[{\"name\": \"prest\"}]"},
+		{"execute update in a table with invalid database", "/0prest/public/test", m, http.StatusBadRequest, ""},
+		{"execute update in a table with invalid schema", "/prest/0public/test", m, http.StatusBadRequest, ""},
+		{"execute update in a table with invalid table", "/prest/public/0test", m, http.StatusBadRequest, ""},
+		{"execute update in a table with invalid where clause", "/prest/public/test?0name=$eq.nuveo", m, http.StatusBadRequest, ""},
+		{"execute update in a table with invalid body", "/prest/public/test?name=$eq.nuveo", nil, http.StatusBadRequest, ""},
 	}
 
 	for _, tc := range testCases {
 		t.Log(tc.description)
-		doRequest(t, server.URL+tc.url, tc.request, "PUT", tc.status, "UpdateTable")
+
+		if tc.body != "" {
+			doRequest(t, server.URL+tc.url, tc.request, "PUT", tc.status, "UpdateTable", tc.body)
+		}
 		doRequest(t, server.URL+tc.url, tc.request, "PATCH", tc.status, "UpdateTable")
 	}
 }
